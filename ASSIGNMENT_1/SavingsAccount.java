c1e0505c9eb7673ccc0b2197886d34a5ea13e3cc
Kaushik Patnaik <146976904+kaushikpatnaik26@users.noreply.github.com>
2024-05-16 16:03:03 +0530

    Update SavingsAccount.java

--- ASSIGNMENT_1/SavingsAccount.java
+++ ASSIGNMENT_1/SavingsAccount.java
@@ -2,7 +2,7 @@ package Q7;
 
 public class SavingsAccount extends Account{
 	
-	final double interestRate=7.5;
+	final double interestRate=12.66;
 	
 	public SavingsAccount(int accountNo, double balance) {
 		super(accountNo,balance);
@@ -11,13 +11,13 @@ public class SavingsAccount extends Account{
 	@Override
 	public void deposit(double amount) {
 		setBalance(amount+getBalance());
-		System.out.println("Interest is:"+0.075*amount);
+		System.out.println("The Interest amount is:"+0.12*amount);
 	}
 	
 	@Override
 	public void withdrawl(double amount) {
 		if(amount>getBalance()) {
-			System.out.println("Insufficient balance.");
+			System.out.println("The balance is insufficient.");
 		}else {
 			setBalance(getBalance()-amount);
 		}
