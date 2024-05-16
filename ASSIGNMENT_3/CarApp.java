0ee20dc3c803762b57081814aaaab9c138879146
Kaushik Patnaik <146976904+kaushikpatnaik26@users.noreply.github.com>
2024-05-16 16:16:27 +0530

    Update CarApp.java

--- ASSIGNMENT_3/CarApp.java
+++ ASSIGNMENT_3/CarApp.java
@@ -6,11 +6,11 @@ import java.util.Comparator;
 public class CarApp {
 	public static void main(String[] args) {
 		ArrayList<Car> list=new ArrayList<>();
-		list.add(new Car("Nano",240,100));
-		list.add(new Car("Alto",125,50));
-		list.add(new Car("Sumo",1225,77));
-		list.add(new Car("WagonR",568,28));
-		list.add(new Car("BMW",111,10));
+		list.add(new Car("lambo",240,100));
+		list.add(new Car("Audi",125,50));
+		list.add(new Car("lotus",1225,77));
+		list.add(new Car("mclauren",568,28));
+		list.add(new Car("ntorq",111,10));
 		
 		Collections.sort(list,Comparator.comparingInt(Car::getStock));
 		
