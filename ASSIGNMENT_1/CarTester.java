072540c0fabe99e4003e745ee9fbe628c76f28a3
Kaushik Patnaik <146976904+kaushikpatnaik26@users.noreply.github.com>
2024-05-16 15:40:16 +0530

    Update CarTester.java

--- ASSIGNMENT_1/CarTester.java
+++ ASSIGNMENT_1/CarTester.java
@@ -3,14 +3,14 @@ package Q1;
 public class CarTester {
     public static void main(String[] args) {
         
-        Car myCar1=new Car("4 Wheels", "Nano");
+        Car myCar1=new Car("4 Wheels", "AUDIII");
         Car myCar2=new Car(null,null);
 
         System.out.println("Car make: "+myCar1.getMake()+","+"Car model: "+myCar1.getModel());
         System.out.println("Car make: "+myCar2.getMake()+","+"Car model: "+myCar2.getModel());
 
-        myCar2.setMake("5 Wheels");
-        myCar2.setModel("Maruti");
+        myCar2.setMake("8 Wheels");
+        myCar2.setModel("lemodghini");
 
 
         System.out.println("Car make: "+myCar2.getMake()+","+"Car model: "+myCar2.getModel());
