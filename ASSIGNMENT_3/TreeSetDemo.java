01f5229825c82992c670ced1a8647e544ed24088
Kaushik Patnaik <146976904+kaushikpatnaik26@users.noreply.github.com>
2024-05-16 16:26:17 +0530

    Update TreeSetDemo.java

--- ASSIGNMENT_3/TreeSetDemo.java
+++ ASSIGNMENT_3/TreeSetDemo.java
@@ -4,17 +4,17 @@ import java.util.TreeSet;
 public class TreeSetDemo {
 	public static void main(String[] args) {
 		TreeSet<Integer> set=new TreeSet<>();
-		set.add(15);
-		set.add(11);
+		set.add(1);
+		set.add(2);
 		set.add(8);
-		set.add(45);
-		set.add(23);
-		set.add(15);
+		set.add(4;
+		set.add(3);
+		set.add(5);
 
 		System.out.println(set);
 		
-		System.out.println(set.contains(15));
-		set.remove(23);
+		System.out.println(set.contains(1));
+		set.remove(3);
 		
 		System.out.println(set);
 	}
