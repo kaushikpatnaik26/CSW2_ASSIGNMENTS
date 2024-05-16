180e558a3b1f7c348ee49538a36c6bca77d5aa9c
Kaushik Patnaik <146976904+kaushikpatnaik26@users.noreply.github.com>
2024-05-16 15:37:25 +0530

    Update Bicycle.java

--- ASSIGNMENT_1/Bicycle.java
+++ ASSIGNMENT_1/Bicycle.java
@@ -4,16 +4,16 @@ public class Bicycle implements Vehicle{
 
 	@Override
 	public void accelerate() {
-		System.out.println("Bicycle is speeding up.");
+		System.out.println("The Bicycle is speeding up.");
 	}
 	
 	public void accelerate(String model) {
-		System.out.println(model+" cycle is accelerating.");
+		System.out.println(model +"The cycle is accelerating.");
 	}
 	
 	@Override
 	public void brake() {
-		System.out.println("Bicycle is applying brakes.");
+		System.out.println("The Bicycle is now applying brakes.");
 	}
 	
 }
