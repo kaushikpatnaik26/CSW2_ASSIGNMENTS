2c5c0a8eb8b8f8d9daa002f67a0c8d2676ff9c1c
Kaushik Patnaik <146976904+kaushikpatnaik26@users.noreply.github.com>
2024-05-16 16:27:19 +0530

    Update Q01.java

--- ASSIGNMENT_4/Q01.java
+++ ASSIGNMENT_4/Q01.java
@@ -2,10 +2,10 @@ package ErrorHandling;
 public class Q01 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String str=null;
+		String s=null;
 		try {
-			int size=str.length();
-			System.out.println("Size of string is :"+size);
+			int size=s.length();
+			System.out.println("Size of string :"+size);
 		}catch(NullPointerException e) {
 			System.err.println(e.getMessage());
 		}
